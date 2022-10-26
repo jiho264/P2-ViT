0960b44500af9d45013edda9cebc1e3222303829
shh <shh>
2022-10-26 17:16:20 +0800

    local_20221026

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=1 python test_quant.py vit_base /home/shared_data/imagenet --quant --ptf --lis --quant-method minmax --mode 1
+CUDA_VISIBLE_DEVICES=0 python test_quant.py deit_base /home/shared_data/imagenet --quant --ptf --lis --quant-method minmax --mode 0
 # CUDA_VISIBLE_DEVICES=0 python test_quant.py vit_base /home/shared_data/imagenet --quant --quant-method minmax --mode 2
